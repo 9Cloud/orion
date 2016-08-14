d67c25e04a14e72a7a4f15245fb56babedfed23c
Kay Sackey <kay@9cloud.us>
2016-08-14 12:09:01 -0400

    Alert  States with Backgrounds

--- orion/guide/interactions.js
+++ orion/guide/interactions.js
@@ -1,13 +1,26 @@
 // React
 import React, {PropTypes} from "react";
 import {StyleGuidePage} from "orion/guide/layout";
-import {Div, Button, Section, SubSection, Spacer, Icon, Notice, VMenuLink} from 'orion/ui/helpers';
+import {Div, Button, Section, SubSection, Spacer, Icon, VMenuLink} from 'orion/ui/helpers';
+import {Notice, Blurb, Success, Info, Warning, Danger, ErrorState, BlankState} from 'orion/ui/helpers';
+
 
 export class Interactions extends StyleGuidePage {
     main() {
         return (
-          <div>
-              <h1 className="section_title"><a name="states">States</a></h1>
+          <Section title="States">
+
+              <SubSection title="With Background">
+                  <Notice>This is a notice.</Notice>
+                  <Blurb>This is a blurb.</Blurb>
+                  <Success>This is a success. No a triumph!</Success>
+                  <Info>This is a informational.</Info>
+                  <Warning>This is a warning. And your last in fact.</Warning>
+                  <Danger>This is a dangerous thing.</Danger>
+                  <ErrorState>This is a state of error.</ErrorState>
+                  <BlankState>This is a blank state.</BlankState>
+              </SubSection>
+
 
               <div className="l-col-5">
                   <h3>Success</h3>
@@ -25,17 +38,19 @@ export class Interactions extends StyleGuidePage {
                   <h3> Blank State </h3>
                   <p className="l-blank-state"> Sorry, no comments yet! </p>
               </div>
+
               <div>
                   <h1 className="section_title"><a name="notices"> Notices</a></h1>
                   <p className="l-notice"> Hello, this is a friendly notice. </p>
               </div>
+
 
               <div>
                   <h1 className="section_title"><a name="animations">Animations</a></h1>
                   <div className="icon-heartbeat"></div>
                   <div className="icon-heart"></div>
               </div>
-          </div>
+          </Section>
         )
     }
 
--- orion/ui/helpers/notices.js
+++ orion/ui/helpers/notices.js
@@ -1,3 +1,4 @@
+import {Component} from 'tide/components';
 import React, {PropTypes} from "react";
 import classNames from "classnames/bind";
 import {Link} from 'react-router';
@@ -5,14 +6,14 @@ import {slugify} from 'orion/utils/string';
 import {Div} from './div';
 
 // Successful action
-export const Success = (props) => <Div className="success" {...props}>{props.children}</Div>;
+
 
 // Action resulted in error
 export const ErrorText = (props) => {
     let withBackground = props.withBackground == true;
 
     if (withBackground) {
-        return <Div className="l-error" {...props}>{props.children}</Div>;
+        return <Div className="l-error"}} />;
     }
     else {
         return <Div className="l-error-txt " {...props}>{props.children}</Div>
@@ -20,8 +21,54 @@ export const ErrorText = (props) => {
 
 };
 
-// Blank State
-export const Blank = (props) => <Div className="l-blank-state" {...props}>{props.children}</Div>;
+//return <Div className="l-error"}} />;
+//return <Div className="l-error-txt " {...props}>{props.children}</Div>
+//export const Success = (props) => <Div className="success"}} />;
+// export const Warning = (props) => <Div className="l-notice"}} />;
+//export const BlankState = (props) => <Div className="l-blank-state"}} />;
 
-// Notice
-export const Notice = (props) => <Div className="l-notice" {...props}>{props.children}</Div>;
+
+class VisibleNotice extends Component {
+    static defaultProps = {
+        textual: false
+    };
+
+    render(){
+        let {textual, style, children,...others} = this.props;
+        let classes = [];
+
+        classes.push("l-alert");
+        classes.push(style.color);
+        classes.push(style.extra);
+
+        // Background
+        if(!textual){
+            classes.push("l-box-shadow");
+            classes.push(style.background);
+        }
+
+        return <Div className="l-alert l-bgcolor--primary--dark" {...others}>{children}</Div>
+    }
+}
+
+// Primary
+export const Notice = (props) => <VisibleNotice {...props} style={{color: "l-bgcolor--primary--dark"}} />;
+
+// Secondary
+export const Blurb = (props) => <VisibleNotice {...props} style={{background: "l-bgcolor--secondary--light"}}/>;
+// Success
+export const Success = (props) => <VisibleNotice {...props} stlye={{extra: "l-success", background: "l-bgcolor--ui-faded-green"}} />;
+// Info
+export const Info = (props) => <VisibleNotice {...props} stlye={{extra: "l-info", background: "l-bgcolor--ui-faded-blue", color: "l-secondary--lighter"}} />;
+// Warning
+export const Warning = (props) => <VisibleNotice {...props} stlye={{extra: "l-notice", color: "l-ui-yellow"}} />;
+// Danger
+export const Danger = (props) => <VisibleNotice {...props} stlye={{extra: "l-danger", background: "l-bgcolor--primary--dark", color: "l-secondary--lighter"}} />;
+// Error
+export const ErrorState = (props) => <VisibleNotice {...props} stlye={{extra: "l-error", background: "l-bgcolor--ui-faded-yellow"}} />;
+export const Error = ErrorState;
+export const ErrorNotice = ErrorState;
+// Blank
+export const BlankState = (props) => <VisibleNotice {...props} stlye={{extra: "l-blank-state"}} />;
+export const Blank = BlankState;
+
