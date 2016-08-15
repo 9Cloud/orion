797beace076416cfc59d1486dad99eeaf6931019
Kekeli Sackey <kekeli@bolddesign.org>
2016-08-15 11:59:51 -0400

    Fix dropdown nav

--- orion/guide/layout.js
+++ orion/guide/layout.js
@@ -9,16 +9,18 @@ export class Navigation extends Component {
     render() {
         return (
             <Header>
-                <NavList>
-                    <NavItem><IndexLink to="/" activeClass="active" onlyActiveOnIndex={true}>Home</IndexLink></NavItem>
-                    <NavItem to="colors">Colors</NavItem>
-                    <NavItem to="forms">Forms</NavItem>
-                    <NavItem to="interactions">Interactions</NavItem>
-                    <NavItem to="helpers">Helpers</NavItem>
-                    <NavItem to="components">UI Components</NavItem>
-                    <NavItem to="nav">Nav</NavItem>
-                    <NavItem to="typography">Typography</NavItem>
-                </NavList>
+                <div className="l-col-push-1 l-col-lg-10">
+                    <NavList>
+                        <NavItem><IndexLink to="/" activeClass="active" onlyActiveOnIndex={true}>Home</IndexLink></NavItem>
+                        <NavItem to="colors">Colors</NavItem>
+                        <NavItem to="forms">Forms</NavItem>
+                        <NavItem to="interactions">Interactions</NavItem>
+                        <NavItem to="helpers">Helpers</NavItem>
+                        <NavItem to="components">UI Components</NavItem>
+                        <NavItem to="nav">Nav</NavItem>
+                        <NavItem to="typography">Typography</NavItem>
+                    </NavList>
+                </div>
             </Header>
         )
     }
--- orion/guide/nav.js
+++ orion/guide/nav.js
@@ -9,7 +9,6 @@ import {Chance} from "chance";
 //import {Chance} from 'chance';
 import {Div, Button, Section, SubSection, Spacer, Icon, Notice, VMenuLink} from 'orion/ui/helpers';
 
-
 export class VirtualScrollExample extends Component {
     overscanRowCount = 0;
     scrollToIndex = 0;
@@ -100,36 +99,33 @@ export class NavComponent extends StyleGuidePage {
         return (
             <div>
                 <h1>Navigation Bar</h1>
+                <p className="l-row-gut-2"> This navigation bar is used globally across the site. </p>
                 <header className="demo-header l-bgcolor--secondary--darker l-col-gut-lg l-clearfix">
                     <div className="pg-container">
                         <a href="/" className="logo l-float-left">Logo</a>
                         <nav>
                             <ul className="l-nav-list">
                                 <li className="l-nav-item"><a href="">Item one</a>
-                                    <ul className="nav-dropdown">
+                                    <ul className="l-nav-dd">
                                         <li>Item One</li>
                                         <li>Item Two</li>
                                         <li>Item Three</li>
-                                        <li>Item Four</li>
                                     </ul>
                                 </li>
                                 <li className="l-nav-item"><a href="">Item two</a></li>
                                 <li className="l-nav-item"><a href="">Item three</a></li>
-
                                 <li className="l-nav-item"><a href="">Item four</a></li>
                                 <li className="l-nav-item"><a href="">Item five</a></li>
-                                <li className="l-nav-item"><a href="">Item six</a></li>
                                 <li className="l-nav-item"><span className="icon-user"></span> Hello User</li>
                                 <li className="l-nav-item"><a href="" className="icon-inbox"></a></li>
                                 <li className="l-nav-item"><a href="" className="icon-bell"></a></li>
                                 <li className="l-nav-item"><a href="" className="icon-flag"></a></li>
                                 <li className="l-nav-item">
                                     <a href="" className="icon-cog"></a>
-                                    <ul className="nav-dropdown">
+                                    <ul className="l-nav-dd">
                                         <li>Item One</li>
                                         <li>Item Two</li>
                                         <li>Item Three</li>
-                                        <li>Item Four</li>
                                     </ul>
                                 </li>
                             </ul>
